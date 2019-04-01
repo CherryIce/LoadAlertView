d008fbf4f1b41f0fa293d2e3411c8f2f2de4d776
Zoombie <a13530836164@163.com>
2019-04-01 16:07:15 +0800

    基本九宫格算法

--- LoadAlertView/LoadAlertView/MainBusiness/FunctionalModule_5/Controller/ICEModule5ViewController.m
+++ LoadAlertView/LoadAlertView/MainBusiness/FunctionalModule_5/Controller/ICEModule5ViewController.m
@@ -8,7 +8,12 @@
 
 #import "ICEModule5ViewController.h"
 
+#define BAI 100
+#define PADDING 10
+
 @interface ICEModule5ViewController ()
+
+@property (nonatomic,strong) UIView * v;
 
 @end
 
@@ -17,6 +22,96 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    [self initUI];
+}
+
+- (void) initUI
+{
+    UIButton * button = [UIButton buttonWithType:UIButtonTypeCustom];
+    [button setFrame:CGRectMake(UIWINDOW_WIDTH/2-BAI/2, Height_NavBar + PADDING, BAI, BAI)];
+    [button addTarget:self action:@selector(chageRandom:) forControlEvents:UIControlEventTouchUpInside];
+    button.backgroundColor = [UIColor darkGrayColor];
+    [self.view addSubview:button];
+}
+
+- (void) chageRandom:(UIButton *)sender
+{
+    int a = (arc4random() % 9) + 1;;
+    
+    NSMutableArray * dataArr = [NSMutableArray array];
+    
+    for (int i = 0; i < a; i++) {
+        [dataArr addObject:@"x"];
+    }
+    
+    [self initWithImageArrLists:[dataArr copy]];
+}
+
+- (void) initWithImageArrLists:(NSArray *)imgArrLists
+{
+    
+    NSLog(@">>>>>>>>%zd",imgArrLists.count);
+    
+    [_v removeFromSuperview];
+    
+    _v = [[UIView alloc] initWithFrame:CGRectMake(0, Height_NavBar + 2 * PADDING + BAI, [UIScreen mainScreen].bounds.size.width, [UIScreen mainScreen].bounds.size.width)];
+    [self.view addSubview:_v];
+    
+    CGFloat width = [UIScreen mainScreen].bounds.size.width/3;
+    switch (imgArrLists.count) {
+        case 1:
+        {
+            //等比例大小
+            UIButton * button = [UIButton buttonWithType:UIButtonTypeCustom];
+            [button setFrame:CGRectMake(PADDING, PADDING, width - PADDING, width - PADDING)];
+            button.backgroundColor = [UIColor greenColor];
+            [_v addSubview:button];
+        }
+            break;
+        case 2:
+        case 4:
+        {
+            //两列,不确定行
+            for(int i = 0; i <  imgArrLists.count / 2; i++)
+            {
+                for(int j = 0; j < 2 ; j++)
+                {
+                    UIButton * button = [UIButton buttonWithType:UIButtonTypeCustom];
+                    [button setFrame:CGRectMake(j*width + PADDING, i*width + PADDING, width - PADDING, width - PADDING)];
+                    button.backgroundColor = [UIColor redColor];
+                    [_v addSubview:button];
+                }
+            }
+        }
+            break;
+        case 3:
+        case 5:
+        case 6:
+        case 7:
+        case 8:
+        case 9:
+        {
+            //三列,不确定行
+            CGFloat num = 1 + imgArrLists.count / 3;
+            for(int i = 0; i < num; i++)
+            {
+                for(int j = 0; j < 3 ; j++)
+                {
+                    UIButton * button = [UIButton buttonWithType:UIButtonTypeCustom];
+                    if (i == num - 1 && j >= imgArrLists.count % 3){
+                        break;
+                    }
+                    [button setFrame:CGRectMake(j*width + PADDING, i*width + PADDING, width - PADDING, width - PADDING)];
+                    button.backgroundColor = [UIColor purpleColor];
+                    [_v addSubview:button];
+                }
+            }
+        }
+            break;
+        default:
+            break;
+    }
 }
 
 /*
